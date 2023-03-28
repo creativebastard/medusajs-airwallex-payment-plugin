d32ffedb281c5e7b9b4a777cbf477bd5b4141cb0
creativebastard <82507977+creativebastard@users.noreply.github.com>
2023-03-28 17:44:48 +0800

    Refactoried a bunch of code
    Had too much duplicate code, refactored and cleaned it up a bit.

--- airwallex.js
+++ airwallex.js
@@ -11,94 +11,83 @@ async function processPayment(payment) {
   const { amount, currency, paymentMethod, card } = payment;
 
   if (paymentMethod === 'card') {
-    // Create a new Airwallex credit card payment intent
-    const paymentIntent = await airwallexClient.paymentIntents.create({
-      amount,
-      currency,
-      payment_method: {
-        card
-      }
-    });
-
-    // If the payment intent requires authentication, return a requires_action status with the next action
-    if (paymentIntent.status === 'REQUIRES_ACTION') {
-      return {
-        status: 'requires_action',
-        action: {
-          type: 'card_verification',
-          payment_intent_id: paymentIntent.id
-        }
-      };
-    }
-
-    // If the payment intent is successful, create a new transaction object in MedusaJS
-    if (paymentIntent.status === 'SUCCEEDED') {
-      const transaction = await Medusa.Transaction.create({
-        amount: paymentIntent.amount,
-        currency: paymentIntent.currency,
-        status: 'captured',
-        payment_id: paymentMethod,
-        provider_id: paymentIntent.id
-      });
-
-      return {
-        status: 'captured',
-        transaction
-      };
-    }
-
-    // If the payment intent has failed, reject the Promise with an error
-    if (paymentIntent.status === 'FAILED') {
-      throw new Error(`Payment failed: ${paymentIntent.failure_message}`);
-    }
-
-    // If the payment intent is in an unknown state, reject the Promise with an error
-    throw new Error(`Unknown payment status: ${paymentIntent.status}`);
+    const paymentIntent = await createCreditCardPaymentIntent(amount, currency, card);
+    return handlePaymentIntent(paymentIntent, paymentMethod);
   } else {
-    // Create a new Airwallex alternative payment intent
-    const paymentIntent = await airwallexClient.paymentIntents.create({
-      amount,
-      currency,
-      payment_method: {
-        type: paymentMethod
-      }
-    });
-
-    // If the payment intent requires authentication, return a requires_action status with the next action
-    if (paymentIntent.status === 'REQUIRES_ACTION') {
-      return {
-        status: 'requires_action',
-        action: {
-          type: 'redirect',
-          url: paymentIntent.next_action.redirect_url
-        }
-      };
-    }
-
-    // If the payment intent is successful, create a new transaction object in MedusaJS
-    if (paymentIntent.status === 'SUCCEEDED') {
-      const transaction = await Medusa.Transaction.create({
-        amount: paymentIntent.amount,
-        currency: paymentIntent.currency,
-        status: 'captured',
-        payment_id: paymentMethod,
-        provider_id: paymentIntent.id
-      });
-
-      return {
-        status: 'captured',
-        transaction
-      };
-    }
-
-    // If the payment intent has failed, reject the Promise with an error
-    if (paymentIntent.status === 'FAILED') {
-      throw new Error(`Payment failed: ${paymentIntent.failure_message}`);
-    }
-
-    // If the payment intent is in an unknown state, reject the Promise with an error
-    throw new Error(`Unknown payment status: ${paymentIntent.status}`);
+    const paymentIntent = await createAlternativePaymentIntent(amount, currency, paymentMethod);
+    return handlePaymentIntent(paymentIntent, paymentMethod);
   }
+}
+
+async function createCreditCardPaymentIntent(amount, currency, card) {
+  return airwallexClient.paymentIntents.create({
+    amount,
+    currency,
+    payment_method: {
+      card
+    }
+  });
+}
+
+async function createAlternativePaymentIntent(amount, currency, paymentMethod) {
+  return airwallexClient.paymentIntents.create({
+    amount,
+    currency,
+    payment_method: {
+      type: paymentMethod
+    }
+  });
+}
+
+function handlePaymentIntent(paymentIntent, paymentMethod) {
+  if (paymentIntent.status === 'REQUIRES_ACTION') {
+    return handleRequiresAction(paymentIntent);
+  }
+
+  if (paymentIntent.status === 'SUCCEEDED') {
+    return handleSuccess(paymentIntent, paymentMethod);
+  }
+
+  throw new Error(`Payment failed: ${paymentIntent.failure_message}`);
+}
+
+function handleRequiresAction(paymentIntent) {
+  if (paymentIntent.next_action.type === 'card_verification') {
+    return {
+      status: 'requires_action',
+      action: {
+        type: 'card_verification',
+        payment_intent_id: paymentIntent.id
+      }
+    };
+  }
+
+  if (paymentIntent.next_action.type === 'redirect') {
+    return {
+      status: 'requires_action',
+      action: {
+        type: 'redirect',
+        url: paymentIntent.next_action.redirect_url
+      }
+    };
+  }
+
+  throw new Error(`Unknown action type: ${paymentIntent.next_action.type}`);
+}
+
+async function handleSuccess(paymentIntent, paymentMethod) {
+  const transaction = await Medusa.Transaction.create({
+    amount: paymentIntent.amount,
+    currency: paymentIntent.currency,
+    status: 'captured',
+    payment_id: paymentMethod,
+    provider_id: paymentIntent.id
+  });
+
+  return {
+    status: 'captured',
+    transaction
+  };
 }
 
 // Export the processPayment and refundPayment functions as a MedusaJS plugin
